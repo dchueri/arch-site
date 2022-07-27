e39544cc68676314829afd613bc275d30e6a0712
dchueri <diegochueri@gmail.com>
2022-07-27 18:31:06 -0400

    vercel config

--- src/components/ProtectedLayout/index.tsx
+++ src/components/ProtectedLayout/index.tsx
@@ -22,7 +22,7 @@ export const ProtectedLayout = ({ children, role }: ProtectedLayoutProps) => {
   const user = getUserLocalStorage();
   const auth = useAuth();
   const history = useNavigate();
-  if(user.role === 'PROJETISTA' && role === 'ADMIN') {
+  if(user.role === 'PROJETISTA' || role === 'ADMIN') {
     return (
       <Container>
         <div style={{height: 'fit-content', width: '100vw'}}>
