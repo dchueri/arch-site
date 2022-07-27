997527fa8539b1362669d6fcad5a9d4fc62caad9
dchueri <diegochueri@gmail.com>
2022-07-27 16:36:44 -0400

    added vercel config

--- src/components/User/UsersList/index.tsx
+++ src/components/User/UsersList/index.tsx
@@ -1,14 +1,23 @@
 import { Button, Stack } from "@mui/material";
+import { NavLink, useNavigate } from "react-router-dom";
 import routesList from "../../../routes/routesList.json";
 import UsersTable from "./UsersTable";
 
 export function UsersList() {
+  const history = useNavigate();
   return (
     <>
       <h1>Usuários</h1>
       <UsersTable />
       <Stack>
-        <Button href={routesList.registerUser} style={{alignSelf: 'center', margin: '2em'}} variant="contained">Cadastrar usuário</Button>
+        <NavLink to={routesList.registerUser}>
+          <Button
+            style={{ alignSelf: "center", margin: "2em" }}
+            variant="contained"
+          >
+            Cadastrar usuário
+          </Button>
+        </NavLink>
       </Stack>
     </>
   );
